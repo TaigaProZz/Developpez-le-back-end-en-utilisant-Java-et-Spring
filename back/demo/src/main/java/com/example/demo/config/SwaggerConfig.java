423d4cf1fcb02d01040dccc0eeed6abc54dbe7e6
Taiga ProZz <flo54@gmx.fr>
2025-03-19 21:04:41 +0100

    (feat) Swagger authentication

--- back/demo/src/main/java/com/example/demo/config/SwaggerConfig.java
+++ back/demo/src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,7 +1,9 @@
 package com.example.demo.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,6 +27,9 @@ public class SwaggerConfig {
                     .title("Mon API")
                     .version("1.0")
                     .description("Documentation de l'API"))
+                    .components(new Components().addSecuritySchemes("bearerAuth",
+                    new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+
             .servers(List.of(new Server().url("/api").description("API")));
   }
 }
