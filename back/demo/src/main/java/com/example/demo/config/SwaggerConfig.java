ec6c1656ff7ce91cec0cae84d775b1d05de52cd1
Taiga ProZz <flo54@gmx.fr>
2025-03-19 22:43:20 +0100

    (fix) Swagger auth token working

--- back/demo/src/main/java/com/example/demo/config/SwaggerConfig.java
+++ back/demo/src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.example.demo.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,8 +25,9 @@ public class SwaggerConfig {
                     .title("Mon API")
                     .version("1.0")
                     .description("Documentation de l'API"))
-                    .components(new Components().addSecuritySchemes("bearerAuth",
-                    new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
-
+                    .components(
+                            new Components().addSecuritySchemes("bearerAuth",
+                            new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT"))
+                    ).addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
   }
 }
