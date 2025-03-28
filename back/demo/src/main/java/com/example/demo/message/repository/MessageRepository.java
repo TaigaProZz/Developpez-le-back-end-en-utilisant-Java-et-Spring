59d0271c55a6a2cbe79f2bf89e84b5e2f7c13ac6
Taiga ProZz <flo54@gmx.fr>
2025-03-28 11:21:29 +0100

    (fix) @Repository for repository

--- back/demo/src/main/java/com/example/demo/message/repository/MessageRepository.java
+++ back/demo/src/main/java/com/example/demo/message/repository/MessageRepository.java
@@ -3,7 +3,9 @@ package com.example.demo.message.repository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.demo.message.model.Message;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MessageRepository extends CrudRepository<Message, Long>  {
   
 }
--- back/demo/src/main/java/com/example/demo/rent/repository/RentRepository.java
+++ back/demo/src/main/java/com/example/demo/rent/repository/RentRepository.java
@@ -4,9 +4,11 @@ import com.example.demo.user.model.User;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.demo.rent.model.Rent;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface RentRepository extends CrudRepository<Rent, Long>  {
   Optional<Rent> findById(Long id);
 }
